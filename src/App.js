3e2d3a4e9980e15600fedd5ea7f362e291bccee1
Daboposki <oscar@graus.biz>
2021-03-18 01:02:39 +0100

    Formulario a medio hacer de registro y actualización

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import Form from './components/Form'
-import Counter from './components/Counter'
 import './App.css';
+import FormCuenta from './components/FormCuenta'
 
 function App() {
+
+  let values = {usuario: "Óscar", correo: "780378@unizar.es", contrasenya: "@#idfa98sfd9"}
+  let valuesNull = {usuario: "", correo: "", contrasenya: ""}
   return (
+    <>
+    <h1 id="title-small">RISK</h1>
     <div className="App">
-      <Form />
+      {/*<Form />
       <hr />
-      <Counter />
+      <Counter />*/}
+      <FormCuenta defaults={valuesNull} submitText='Registrarse'/>
+      <FormCuenta defaults={values} submitText='Actualizar'/>
     </div>
+    </>
   );
 }
 
