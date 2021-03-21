16c3260c99f5733b9df4d7b84e0f1b11a35e95a5
alecron <alecronster@gmail.com>
2021-03-21 17:06:33 +0100

    Revert "Arreglos css del formulario de registro; retirado style css de los imports"
    This reverts commit 99634f0ea5ed83d999e840f74dde4096d9c2ad4d.

--- src/components/sesion/FormCuenta.js
+++ src/components/sesion/FormCuenta.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import "./formCuenta.css";
+import "../../assets/css/style.css";
 
 /**
  * Representa un formulario de registro de la cuenta, si algún valor es incorrecto
@@ -8,100 +8,100 @@ import "./formCuenta.css";
  * Requisitos:
  * - Nombre de usuario no vacío
  * - Correo con formato de correo
- * - Contraseña de mínimo 8 caracteres, con al menos una mayúscula, minúscula,
+ * - Contraseña de mínimo 8 caracteres, con al menos una mayúscula, minúscula, 
  * 		dígito y carácter especial.
  * @param {ObjectSesion} defaults Valores por defecto del formulario
- * @param {string} submitText Texto del botón de enviar formulario
+ * @param {string} submitText Texto del botón de enviar formulario 
  * @param {función} handleOnSubmit Función ejecutada al enviar el formulario
  */
 export const FormCuenta = ({ defaults, submitText, handleOnSubmit }) => {
-  const { register, handleSubmit, errors } = useForm({
-    defaultValues: {
-      nombre: defaults.nombre ? defaults.nombre : "",
-      correo: defaults.correo ? defaults.correo : "",
-      clave: defaults.clave ? defaults.clave : "",
-      recibeCorreos: defaults.recibeCorreos ? defaults.recibeCorreos : false,
-    },
-  });
+	const { register, handleSubmit, errors } = useForm({
+		defaultValues: {
+			nombre: defaults.nombre ? defaults.nombre : "",
+			correo: defaults.correo ? defaults.correo : "",
+			clave: defaults.clave ? defaults.clave : "",
+			recibeCorreos: defaults.recibeCorreos ? defaults.recibeCorreos : false,
+		},
+	});
 
-  return (
-    <div className="container cont-reg">
-      <form
-        id="registro"
-        onSubmit={handleSubmit((formData) => {
-          handleOnSubmit(formData);
-        })}
-      >
-        <div className="form-row">
-          <label htmlFor="nombre"> Nombre de usuario</label>
-          <input
-            type="text"
-            name="nombre"
-            id="nombre"
-            placeholder="Nombre de usuario"
-            ref={register({ required: "Este campo no se puede dejar vacío." })}
-          />
-          {errors.nombre ? <div> {errors.nombre.message}</div> : null}
-        </div>
+	return (
+		<div className="container cont-reg">
+			<form
+				id="registro"
+				onSubmit={handleSubmit((formData) => {
+					handleOnSubmit(formData);
+				})}
+			>
+				<div className="form-row">
+					<label htmlFor="nombre"> Nombre de usuario</label>
+					<input
+						type="text"
+						name="nombre"
+						id="nombre"
+						placeholder="Nombre de usuario"
+						ref={register({ required: "Este campo no se puede dejar vacío." })}
+					/>
+					{errors.nombre ? <div> {errors.nombre.message}</div> : null}
+				</div>
 
-        <div className="form-row">
-          <label htmlFor="correo"> Correo electrónico</label>
-          <input
-            type="email"
-            name="correo"
-            id="correo"
-            placeholder="Correo"
-            ref={register({ required: "Este campo no se puede dejar vacío." })}
-          />
-          {errors.correo ? <div> {errors.correo.message}</div> : null}
-        </div>
+				<div className="form-row">
+					<label htmlFor="correo"> Correo electrónico</label>
+					<input
+						type="email"
+						name="correo"
+						id="correo"
+						placeholder="Correo"
+						ref={register({ required: "Este campo no se puede dejar vacío." })}
+					/>
+					{errors.correo ? <div> {errors.correo.message}</div> : null}
+				</div>
 
-        <div className="form-row">
-          <label htmlFor="contrasenya"> Contraseña</label>
-          <input
-            type="password"
-            name="clave"
-            id="clave"
-            placeholder="Contraseña"
-            ref={register({
-              required: "Este campo no se puede dejar vacío.",
-              minLength: {
-                value: 8,
-                message: "Tiene que haber mínimo 8 caracteres.",
-              },
-              validate: (value) => {
-                return (
-                  [/[a-z]/, /[A-Z]/, /[0-9]/, /[^a-zA-Z0-9]/].every((pattern) =>
-                    pattern.test(value)
-                  ) ||
-                  "La contraseña debe incluir al menos una mayúscula, minúscula, un número y un carácter especial."
-                );
-              },
-            })}
-          />
-          {errors.clave ? <div> {errors.clave.message}</div> : null}
-        </div>
+				<div className="form-row">
+					<label htmlFor="contrasenya"> Contraseña</label>
+					<input
+						type="password"
+						name="clave"
+						id="clave"
+						placeholder="Contraseña"
+						ref={register({
+							required: "Este campo no se puede dejar vacío.",
+							minLength: {
+								value: 8,
+								message: "Tiene que haber mínimo 8 caracteres.",
+							},
+							validate: (value) => {
+								return (
+									[/[a-z]/, /[A-Z]/, /[0-9]/, /[^a-zA-Z0-9]/].every((pattern) =>
+										pattern.test(value)
+									) ||
+									"La contraseña debe incluir al menos una mayúscula, minúscula, un número y un carácter especial."
+								);
+							},
+						})}
+					/>
+					{errors.clave ? <div> {errors.clave.message}</div> : null}
+				</div>
 
-        <div className="form-row">
-          <label htmlFor="recibeCorreos"> Quiero recibir correo: </label>
-          <input
-            type="checkbox"
-            name="recibeCorreos"
-            id="recibeCorreos"
-            ref={register()}
-          />
-        </div>
-        <div className="nav-buttons">
-          <div className="bttn btn-nav">
-            <button type="button">Atrás</button>
-          </div>
-          <div className="bttn btn-nav">
-            <button type="submit">{submitText}</button>
-          </div>
-        </div>
-      </form>
-    </div>
-  );
+				<div className="form-row">
+					<label htmlFor="recibeCorreos"> Quiero recibir correo: </label>
+					<input
+						type="checkbox"
+						name="recibeCorreos"
+						id="recibeCorreos"
+						ref={register()}
+					/>
+				</div>
+				<div className="nav-buttons">
+					<div className="btn btn-nav">
+						<button type="button">Atrás</button>
+					</div>
+					<div className="btn btn-nav">
+						<button type="submit">{submitText}</button>
+					</div>
+				</div>
+			</form>
+		</div>
+	);
 };
 
 export default FormCuenta;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 //import ListaAmigos from './components/panelAmigos/ListaAmigos';
-import reportWebVitals from "./reportWebVitals";
-//import './assets/css/style.css'
+import reportWebVitals from './reportWebVitals';
+import './assets/css/style.css'
 
 //const amigos = [{nombre: "Jose"}, {nombre: "Pepe"}] //Variable de prueba
 /**
@@ -16,7 +16,7 @@ ReactDOM.render(
     <App />
     {/*<ListaAmigos usuarios={amigos}/>*/}
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
