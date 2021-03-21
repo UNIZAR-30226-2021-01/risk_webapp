99634f0ea5ed83d999e840f74dde4096d9c2ad4d
alecron <alecronster@gmail.com>
2021-03-21 16:31:04 +0100

    Arreglos css del formulario de registro; retirado style css de los imports

--- src/App.js
+++ src/App.js
@@ -1,62 +1,60 @@
-import React, {useState, useContext} from 'react';
-import Cookies from 'js-cookie'
-import './index.css';
-import constants from './constants.js';
+import React, { useState, useContext } from "react";
+import Cookies from "js-cookie";
+import "./index.css";
+import constants from "./constants.js";
 import {
-	Route, 
-	BrowserRouter as Router, 
-	Switch, 
-	Redirect, 
-	//Link 
-} from "react-router-dom"
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+  //Link
+} from "react-router-dom";
 
-import Head from "./components/Head"
-import Header from "./components/Header"
-import Footer from "./components/Footer"
+import Head from "./components/Head";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 //import FormCuenta from "./components/FormCuenta";
-import './assets/css/style.css'
+//import "./assets/css/style.css";
 
-import AuthApi from "./components/sesion/AuthApi"
-import Registrar from "./components/sesion/Registrar"
-import InicioSesion from "./components/sesion/InicioSesion"
+import AuthApi from "./components/sesion/AuthApi";
+import Registrar from "./components/sesion/Registrar";
+import InicioSesion from "./components/sesion/InicioSesion";
 
-
-/**	
+/**
  * App contiene el router y el header/footer de la aplicación,
  * previene al usuario de entrar a las páginas en las que se requiere
  * estar loggeado si no lo está y viceversa.
  * @todo Darle bien estilo a todo.
  */
 function App() {
+  const [auth, setAuth] = useState(constants.NULL_VALUES);
 
-	const [auth, setAuth] = useState(constants.NULL_VALUES)
-
-	/**
-	 * Lee las cookies de usuario, y si las hay establece al usuario como
-	 * loggeado.
-	 */
-	const readCookie = () =>{
-		const user = Cookies.get(constants.COOKIE_USER);
-		if (user){
-			let data = JSON.parse(user)
-			data.logged = true
-			setAuth(data);
-		}
-	}
-	React.useEffect(() =>{
-		readCookie();
-	}, [])
+  /**
+   * Lee las cookies de usuario, y si las hay establece al usuario como
+   * loggeado.
+   */
+  const readCookie = () => {
+    const user = Cookies.get(constants.COOKIE_USER);
+    if (user) {
+      let data = JSON.parse(user);
+      data.logged = true;
+      setAuth(data);
+    }
+  };
+  React.useEffect(() => {
+    readCookie();
+  }, []);
 
   return (
-	<div>
-		<Head />
-		<AuthApi.Provider value={{auth,setAuth}}>
-  <Router>
-		<div className="wrapper">
-		<Header />
-		<hr />
-		<Routes />
-		{/*
+    <div>
+      <Head />
+      <AuthApi.Provider value={{ auth, setAuth }}>
+        <Router>
+          <div className="wrapper">
+            <Header />
+            <hr />
+            <Routes />
+            {/*
 		<Switch>
 			<Route exact path="/" component={Header} />
 			<Route exact path="/init" component={Header} />
@@ -68,11 +66,11 @@ function App() {
 
 		</Switch>
 			*/}
-		</div>
-	<Footer />
-  </Router>
-		</AuthApi.Provider> 
-  </div>
+          </div>
+          <Footer />
+        </Router>
+      </AuthApi.Provider>
+    </div>
   );
 }
 
@@ -80,73 +78,72 @@ function App() {
  * Routes contiene todas las direcciones de la aplicación y qué componente
  * se ha de renderizar de manera segura.
  */
-const Routes = () =>{
-	const Auth = useContext(AuthApi)
-	return(
-		<Switch>
-			<ProtectedLogin path="/registro" auth={Auth.auth.logged} component={Registrar} />
-			<ProtectedLogin path="/inicioSesion" auth={Auth.auth.logged} component={InicioSesion} />
-			<ProtectedRoute path="/dashboard" auth={Auth.auth.logged}  component={Dashboard} />
-			<ProtectedRoute path="/" auth={Auth.auth.logged}  component={Dashboard} />
-		</Switch>
-	)
-}
+const Routes = () => {
+  const Auth = useContext(AuthApi);
+  return (
+    <Switch>
+      <ProtectedLogin
+        path="/registro"
+        auth={Auth.auth.logged}
+        component={Registrar}
+      />
+      <ProtectedLogin
+        path="/inicioSesion"
+        auth={Auth.auth.logged}
+        component={InicioSesion}
+      />
+      <ProtectedRoute
+        path="/dashboard"
+        auth={Auth.auth.logged}
+        component={Dashboard}
+      />
+      <ProtectedRoute path="/" auth={Auth.auth.logged} component={Dashboard} />
+    </Switch>
+  );
+};
 
 /**
  * ProtectedRoute protege a una ruta de tratar de entrar si no
  * se está loggeado en el sistema.
  */
-const ProtectedRoute = ({auth, component:Component, ...rest}) =>{
-	return(
-		<Route
-		{...rest} 
-		render ={()=>auth?(
-			<Component/>
-		):
-		(
-			<Redirect to="/registro"/>
-		)
-	}
-		/>
-	)
-}
+const ProtectedRoute = ({ auth, component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={() => (auth ? <Component /> : <Redirect to="/registro" />)}
+    />
+  );
+};
 
 /**
  * ProtectedLogin protege a una ruta de tratar de entrar si
  * se está loggeado en el sistema.
  */
-const ProtectedLogin = ({auth, component:Component, ...rest}) =>{
-	return(
-		<Route
-		{...rest} 
-		render ={()=>!auth?(
-			<Component/>
-		):
-		(
-			<Redirect to="/dashboard"/>
-		)
-	}
-		/>
-	)
-}
+const ProtectedLogin = ({ auth, component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={() => (!auth ? <Component /> : <Redirect to="/dashboard" />)}
+    />
+  );
+};
 
 /**
  * Dashboard es una prueba de deslogeo, realmente no es necesario,
  * corresponderá al menú principal una vez loggeado
  */
-const Dashboard = () =>{
-	const Auth = useContext(AuthApi)
-	const handleOnClick = ()=>{
-		Auth.setAuth(constants.NULL_VALUES);
-		Cookies.remove(constants.COOKIE_USER);
-	}
-	return(
-		<div>
-			<h1>Dashboard</h1>
-			<button onClick={handleOnClick}>logout</button>
-		</div>
-	)
-}
-
+const Dashboard = () => {
+  const Auth = useContext(AuthApi);
+  const handleOnClick = () => {
+    Auth.setAuth(constants.NULL_VALUES);
+    Cookies.remove(constants.COOKIE_USER);
+  };
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <button onClick={handleOnClick}>logout</button>
+    </div>
+  );
+};
 
 export default App;
