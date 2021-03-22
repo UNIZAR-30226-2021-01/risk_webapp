874ceb28e8d41deb817714a8f22d9f27ee265072
alecron <alecronster@gmail.com>
2021-03-22 01:06:02 +0100

    Componente opciones de menu y estilos header y footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,8 @@ import React, {useContext} from 'react'
 import AuthApi from "./sesion/AuthApi"
 import constants from './../constants.js'
 import Cookies from 'js-cookie'
+import "./header.css"
+import MenuOpts from "./MenuOpts"
 
 /**
  * Header contiene el encabezado de la aplicación, si está loggeado
@@ -16,19 +18,20 @@ export const Header = () => {
 	}
 
 	return (
-		<nav className="navbar navbar-dark top header">
-			<div className="container d-flex justify-content-between" >
-				<a href="init" className="navbar-brand d-flex align-items-center">
+		<nav className="nav-header">
+			<div className="container cont-navbar" >
+				<a href="init" className="navbar-brand">
 					<strong>RISK</strong>
 				</a>
-				{Auth.auth.logged && 
+
+				{/*Auth.auth.logged && 
 				<div className="dropdown">
 					<button className="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{Auth.auth.usuario.nombre}</button>
 					<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
 						<button className="dropdown-item" onClick={logOut}>Cerrar sesión</button>
 					</div>
 				</div>
-				}
+				*/}
 			</div>
 		</nav>
 	)
