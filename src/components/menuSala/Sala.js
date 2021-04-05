1a65735c5a7d0ac46314aa4d202989a4bd89bf71
alecron <alecronster@gmail.com>
2021-04-05 18:04:43 +0200

    Ruta sala pero no funciona aún

--- src/components/menuSala/Sala.js
+++ src/components/menuSala/Sala.js
@@ -1,3 +1,17 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
 /**
  * @todo link en el router
  */
+
+export const Sala = (props) => {
+	const { id } = useParams()
+
+	return (
+		<>
+			<p>{id}</p>
+		</>
+	)
+}
+
+export default Sala
