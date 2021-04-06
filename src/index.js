1209967a75bce76f3a5bd19d9ffe5788da2081d1
Daboposki <oscar@graus.biz>
2021-04-06 16:48:44 +0200

    Quitado strictmode por api de material-ui

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import App from './App'
  * @requires App.js
  */
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+	//<React.StrictMode>
+	<App />,
+	//</React.StrictMode>,
 	document.getElementById('root')
 )
 // If you want to start measuring performance in your app, pass a function
