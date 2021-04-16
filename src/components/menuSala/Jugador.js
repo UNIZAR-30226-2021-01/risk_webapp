35d87382cfb4f1c50009bae43499ea24e3b59335
alecron <alecronster@gmail.com>
2021-04-17 00:48:52 +0200

    Estilos sal de creacion de partida (jugador y botones) terminados

--- src/components/menuSala/Jugador.js
+++ src/components/menuSala/Jugador.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types'
 import { MDBRow, MDBCol } from 'mdbreact'
 import tropas from '../../assets/tropas/tropas'
 import iconos from '../../assets/iconos/iconos'
+import marcoIcono from '../../assets/UI/avatar_marco.png'
+import fondoAspecto from '../../assets/UI/avatar_fondo.png'
+import './Jugador.css'
 
 /**
  * Representación de un jugador para la lista de jugadores en
@@ -15,8 +18,9 @@ export const Jugador = ({ datos }) => {
 	return (
 		<MDBRow>
 			<MDBCol md="4">
-				<div className="imagenPrueba">
-					<img src={icono} />
+				<div>
+					<img src={marcoIcono} className="icono-marco-jugador"></img>
+					<img src={icono} className="icono-jugador" />
 				</div>
 			</MDBCol>
 			<MDBCol
@@ -26,8 +30,9 @@ export const Jugador = ({ datos }) => {
 				{datos.nombre}
 			</MDBCol>
 			<MDBCol md="4">
-				<div className="imagenPrueba">
-					<img src={aspecto} />
+				<div>
+					<img src={aspecto} className="aspecto-jugador" />
+					<img src={fondoAspecto} className="aspecto-fondo-jugador"></img>
 				</div>
 			</MDBCol>
 		</MDBRow>
