91d16d44304115c912945a259d2377ee6da2de08
Daboposki <oscar@graus.biz>
2021-04-19 13:38:57 +0200

    Terminada máquina de estados, sin acciones de comunicación con el servidor.

--- src/components/partida/partidaEstados.js
+++ src/components/partida/partidaEstados.js
@@ -1,14 +1,6 @@
 import { useReducer } from 'react'
 import Risk from 'assets/mapas/RiskMapa'
 
-export const MAPEO_TIPO_ACCIONES = {
-	p: ACCIONES.DATOS_COMPLETOS_PARTIDA,
-	f: ACCIONES.CONFIRMAR_CAMBIO_FASE,
-	r: ACCIONES.CONFIRMACION_REFUERZO,
-	a: ACCIONES.CONFIRMACION_ATAQUE,
-	m: ACCIONES.CONFIRMACION_MOVIMIENTO,
-}
-
 export const JUGADAS = {
 	REFUERZO: 'REFUERZO',
 	ATAQUE: 'ATAQUE',
@@ -18,12 +10,17 @@ export const JUGADAS = {
 export const ACCIONES = {
 	ERROR: 'ERROR',
 	ERROR_GLOBAL: 'ERROR_GLOBAL',
+	CANCELAR: 'CANCELAR',
 
 	DATOS_COMPLETOS_PARTIDA: 'DATOS_COMPLETOS_PARTIDA',
 	TOCA_MI_TURNO: 'MI_TURNO',
 
 	ENVIAR_MENSAJE_CAMBIO_FASE: 'MENSAJE_CAMBIO_FASE',
 	CONFIRMACION_CAMBIO_FASE: 'CONFIRMACION_CAMBIO_FASE',
+
+	SELECCIONAR_ORIGEN: 'SELECCIONAR_ORIGEN',
+	SELECCIONAR_DESTINO: 'SELECCIONAR_DESTINO',
+	SELECCIONAR_UNIDADES: 'SELECCIONAR_UNIDADES',
 
 	ENVIAR_MENSAJE_REFUERZO: 'ENVIAR_MENSAJE_REFUERZO',
 	CONFIRMACION_REFUERZO: 'CONFIRMACION_REFUERZO',
@@ -33,6 +30,14 @@ export const ACCIONES = {
 
 	ENVIAR_MENSAJE_MOVIMIENTO: 'ENVIAR_MENSAJE_MOVIMIENTO',
 	CONFIRMACION_MOVIMIENTO: 'CONFIRMACION_MOVIMIENTO',
+}
+
+export const MAPEO_TIPO_ACCIONES = {
+	p: ACCIONES.DATOS_COMPLETOS_PARTIDA,
+	f: ACCIONES.CONFIRMAR_CAMBIO_FASE,
+	r: ACCIONES.CONFIRMACION_REFUERZO,
+	a: ACCIONES.CONFIRMACION_ATAQUE,
+	m: ACCIONES.CONFIRMACION_MOVIMIENTO,
 }
 
 export const FASES = {
@@ -46,15 +51,24 @@ export const ESTADOS = {
 	TURNO_RIVAL: 'TURNO_RIVAL',
 
 	FASE_DE_REFUERZOS: 'FASE DE REFUERZOS',
+	FASE_DE_REFUERZOS_SELECCIONADO_DESTINO:
+		'FASE DE REFUERZOS SELECCIONADO DESTINO',
 	ESPERANDO_CONFIRMACION_REFUERZO: 'ESPERANDO CONFIRMACION REFUERZO',
 	CAMBIO_DE_FASE_A_ATAQUE: 'CAMBIO DE FASE A ATAQUE',
 
 	FASE_DE_ATAQUE: 'FASE DE ATAQUE',
+	FASE_DE_ATAQUE_SELECCIONADO_ORIGEN: 'FASE DE ATAQUE SELECCIONADO ORIGEN',
+	FASE_DE_ATAQUE_SELECCIONADO_DESTINO: 'FASE DE ATAQUE SELECCIONADO DESTINO',
 	ESPERANDO_CONFIRMACION_ATAQUE: 'ESPERANDO CONFIRMACION ATAQUE',
 	CAMBIO_DE_FASE_A_MOVIMIENTO: 'CAMBIO DE FASE A MOVIMIENTO',
 
 	FASE_DE_MOVIMIENTO: 'FASE DE MOVIMIENTO',
+	FASE_DE_MOVIMIENTO_SELECCIONADO_ORIGEN:
+		'FASE DE MOVIMIENTO SELECCIONADO ORIGEN',
+	FASE_DE_MOVIMIENTO_SELECCIONADO_DESTINO:
+		'FASE DE MOVIMIENTO SELECCIONADO DESTINO',
 	ESPERANDO_CONFIRMACION_MOVIMIENTO: 'ESPERANDO CONFIRMACION MOVIMIENTO',
+
 	PASAR_TURNO: 'PASAR TURNO',
 }
 
@@ -64,6 +78,92 @@ const FASE_ESTADO = [
 	ESTADOS.FASE_DE_ATAQUE,
 	ESTADOS.FASE_DE_MOVIMIENTO,
 ]
+
+//Considerar lanzar excepciones para debuggear
+function estadoPrevio(estado) {
+	switch (estado) {
+		case ESTADOS.FASE_DE_REFUERZOS_SELECCIONADO_DESTINO:
+			return ESTADOS.FASE_DE_REFUERZOS
+		case ESTADOS.FASE_DE_ATAQUE_SELECCIONADO_ORIGEN:
+			return ESTADOS.FASE_DE_ATAQUE
+		case ESTADOS.FASE_DE_ATAQUE_SELECCIONADO_DESTINO:
+			return ESTADOS.FASE_DE_ATAQUE_SELECCIONADO_ORIGEN
+		case ESTADOS.FASE_DE_MOVIMIENTO_SELECCIONADO_ORIGEN:
+			return ESTADOS.FASE_DE_MOVIMIENTO
+		case ESTADOS.FASE_DE_MOVIMIENTO_SELECCIONADO_DESTINO:
+			return ESTADOS.FASE_DE_MOVIMIENTO_SELECCIONADO_ORIGEN
+		case ESTADOS.CAMBIO_DE_FASE_A_ATAQUE:
+			return ESTADOS.FASE_DE_REFUERZOS
+		case ESTADOS.CAMBIO_DE_FASE_A_MOVIMIENTO:
+			return ESTADOS.FASE_DE_ATAQUE
+		case ESTADOS.PASAR_TURNO:
+			return ESTADOS.FASE_DE_MOVIMIENTO
+		default:
+			return estado
+	}
+}
+
+function estadoSiguienteSeleccionarOrigen(estado) {
+	switch (estado) {
+		case ESTADOS.FASE_DE_MOVIMIENTO:
+			return ESTADOS.FASE_DE_MOVIMIENTO_SELECCIONADO_ORIGEN
+		case ESTADOS.FASE_DE_ATAQUE:
+			return ESTADOS.FASE_DE_ATAQUE_SELECCIONADO_ORIGEN
+		default:
+			return estado
+	}
+}
+
+function estadoSiguienteSeleccionarDestino(estado) {
+	switch (estado) {
+		case ESTADOS.FASE_DE_REFUERZOS:
+			return ESTADOS.FASE_DE_REFUERZOS_SELECCIONADO_DESTINO
+		case ESTADOS.FASE_DE_ATAQUE_SELECCIONADO_ORIGEN:
+			return ESTADOS.FASE_DE_ATAQUE_SELECCIONADO_DESTINO
+		case ESTADOS.FASE_DE_MOVIMIENTO_SELECCIONADO_ORIGEN:
+			return ESTADOS.FASE_DE_MOVIMIENTO_SELECCIONADO_DESTINO
+		default:
+			return estado
+	}
+}
+
+function estadoSiguienteSeleccionarUnidades(estado) {
+	switch (estado) {
+		case ESTADOS.FASE_DE_REFUERZOS_SELECCIONADO_DESTINO:
+			return ESTADOS.ESPERANDO_CONFIRMACION_REFUERZO
+		case ESTADOS.FASE_DE_ATAQUE_SELECCIONADO_DESTINO:
+			return ESTADOS.ESPERANDO_CONFIRMACION_ATAQUE
+		case ESTADOS.FASE_DE_MOVIMIENTO_SELECCIONADO_DESTINO:
+			return ESTADOS.ESPERANDO_CONFIRMACION_MOVIMIENTO
+		default:
+			return estado
+	}
+}
+
+function estadoACambioFase(estado) {
+	switch (estado) {
+		case ESTADOS.FASE_DE_REFUERZOS:
+			return ESTADOS.CAMBIO_DE_FASE_A_ATAQUE
+		case ESTADOS.FASE_DE_ATAQUE:
+			return ESTADOS.CAMBIO_DE_FASE_A_MOVIMIENTO
+		case ESTADOS.FASE_DE_MOVIMIENTO:
+			return ESTADOS.PASAR_TURNO
+		default:
+			return estado
+	}
+}
+
+function estadoSigCambioFase(estado) {
+	switch (estado) {
+		case ESTADOS.CAMBIO_DE_FASE_A_ATAQUE:
+			return ESTADOS.FASE_DE_ATAQUE
+		case ESTADOS.CAMBIO_DE_FASE_A_MOVIMIENTO:
+			return ESTADOS.FASE_DE_MOVIMIENTO
+		default:
+			return estado
+	}
+}
+
 /*
 	Contenido del estado
 {
@@ -71,6 +171,10 @@ const FASE_ESTADO = [
 	"ultimaJugada": {
 		// Lo que sea
 	}
+	"datosJugadaActual": {
+
+	}
+	error: "" (solo si hay)
 	"tiempoTurno": int,
 	"nombreSala": string,
 	"turnoActual": int,
@@ -85,7 +189,7 @@ const initialState = { estadoInterno: ESTADOS.CARGANDO }
 
 function casosLocales(state, action) {
 	switch (action.tipo) {
-		case ESTADOS.CONFIRMACION_REFUERZO: {
+		case ACCIONES.CONFIRMACION_REFUERZO: {
 			let idTerritorio = action.data.id
 			state.ultimaJugada = {
 				jugada: JUGADAS.REFUERZO,
@@ -97,10 +201,18 @@ function casosLocales(state, action) {
 			}
 			return state
 		}
-		case ESTADOS.CONFIRMACION_ATAQUE: {
-			break
+		case ACCIONES.CONFIRMACION_ATAQUE: {
+			let idTerritorioOrigen = action.data.territorioOrigen.id
+			let idTerritorioDestino = action.data.territorioDestino.id
+			state.ultimaJugada = {
+				jugada: JUGADAS.ATAQUE,
+				...action.data,
+			}
+			state.territorios[idTerritorioOrigen] = action.data.territorioOrigen
+			state.territorios[idTerritorioDestino] = action.data.territorioDestino
+			return state
 		}
-		case ESTADOS.CONFIRMACION_MOVIMIENTO: {
+		case ACCIONES.CONFIRMACION_MOVIMIENTO: {
 			let idTerritorioOrigen = action.data.territorioOrigen.id
 			let idTerritorioDestino = action.data.territorioDestino.id
 			state.ultimaJugada = {
@@ -114,8 +226,61 @@ function casosLocales(state, action) {
 			}
 			return state
 		}
-		case ESTADOS.CONFIRMACION_CAMBIO_FASE: {
-			break
+		case ACCIONES.CANCELAR: {
+			return { ...state, estadoInterno: estadoPrevio(state.estadoInterno) }
+		}
+		case ACCIONES.ERROR: {
+			return {
+				...state,
+				estadoInterno: estadoPrevio(state.estadoInterno),
+				error: action.data.err,
+			}
+		}
+		case ACCIONES.SELECCIONAR_ORIGEN: {
+			return {
+				...state,
+				datosJugadaActual: {
+					origen: action.data.datosExtra,
+				},
+				estadoInterno: estadoSiguienteSeleccionarOrigen(state.estadoInterno),
+			}
+		}
+		case ACCIONES.SELECCIONAR_DESTINO: {
+			let datosJugadaActual = {}
+			if (state.estadoInterno !== ESTADOS.FASE_REFUERZOS) {
+				datosJugadaActual.idOrigen = state.datosJugadaActual.idOrigen
+			}
+			return {
+				...state,
+				datosJugadaActual: {
+					...datosJugadaActual,
+					destino: action.data.datosExtra,
+				},
+				estadoInterno: estadoSiguienteSeleccionarDestino(state.estadoInterno),
+			}
+		}
+		case ACCIONES.SELECCIONAR_UNIDADES: {
+			return {
+				...state,
+				datosJugadaActual: {
+					...state.datosJugadaActual,
+					tropas: action.data.datosExtra,
+				},
+				estadoInterno: estadoSiguienteSeleccionarUnidades(state.estadoInterno),
+			}
+		}
+		case ACCIONES.ENVIAR_MENSAJE_CAMBIO_FASE: {
+			delete state.datosJugadaActual
+			return {
+				...state,
+				estadoInterno: estadoACambioFase(state.estadoInterno),
+			}
+		}
+		case ACCIONES.CONFIRMACION_CAMBIO_FASE: {
+			return {
+				...state,
+				estadoInterno: estadoSigCambioFase(state.estadoInterno),
+			}
 		}
 	}
 }
@@ -123,7 +288,8 @@ function casosLocales(state, action) {
 /*
 	Action recibida
 	tipo: "",
-	tengoTurno: "",
+	tengoTurno: bool,
+	datosExtra: , (id origen, id destino, ntropas...) No poner el campo, solo el número
 	"data": {
 		"_tipoMensaje": "p",
 		"tiempoTurno": int,
@@ -134,10 +300,15 @@ function casosLocales(state, action) {
 		"ultimoTurno": ISO8601,
 		"jugadores": [ { "id":int, "nombre":string, "icono":int, "aspecto":int, "sigueVivo":bool, "refuerzos":int, } ],
 		"territorios": [ { "id":int, "jugador":int, "tropas":int } ],
+
+		o
+
+		el mensaje que mande el server
 	}
 */
 function maquinaEstados(state, action) {
 	delete action.data._tipoMensaje
+	delete state.error
 	if (action.tipo === ESTADOS.DATOS_COMPLETOS_PARTIDA) {
 		if (action.tengoTurno) {
 			action.data.estado = FASE_ESTADO[action.data.fase]
