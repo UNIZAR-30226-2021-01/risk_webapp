24234be48654e5bc5d8b5b035bf5ebf4fc334819
alecron <alecronster@gmail.com>
2021-04-24 16:40:33 +0200

    Marcos de cada jugador de su color. De remarca el jugador del que es el turno

--- src/components/partida/ListaJugadores.js
+++ src/components/partida/ListaJugadores.js
@@ -2,14 +2,22 @@ import React from 'react'
 import { MDBListGroup, MDBListGroupItem } from 'mdbreact'
 import JugadorPartida from './JugadorPartida'
 
-const ListaJugadores = ({ jugadores }) => {
+const ListaJugadores = ({ jugadores, jugadorTurno }) => {
 	return (
 		<MDBListGroup className="jugadores-en-partida">
-			{jugadores.map((jugador) => (
-				<MDBListGroupItem key={jugador.id.toString()}>
-					<JugadorPartida jugador={jugador} />
-				</MDBListGroupItem>
-			))}
+			{jugadores.map((jugador, index) => {
+				let clasesJugador = `jugador${index}`
+				if (jugador.id === jugadorTurno)
+					clasesJugador = clasesJugador + ' esSuTurno'
+				return (
+					<MDBListGroupItem
+						key={jugador.id.toString()}
+						className={clasesJugador}
+					>
+						<JugadorPartida jugador={jugador} />
+					</MDBListGroupItem>
+				)
+			})}
 		</MDBListGroup>
 	)
 }
