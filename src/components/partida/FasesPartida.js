b51b59eb84db740afc6f4cb1e104fefaff29bed5
alecron <alecronster@gmail.com>
2021-04-30 22:38:38 +0200

    flechas de fase close #74

--- src/components/partida/FasesPartida.js
+++ src/components/partida/FasesPartida.js
@@ -1,29 +1,25 @@
 import React from 'react'
 import './FasesPartida.css'
 
-const FasesPartida = () => {
+const FasesPartida = ({ fase }) => {
+	let clases = ['', '', '']
+
+	switch (fase) {
+		case 1:
+			clases = ['activa', '', '']
+			break
+		case 2:
+			clases = ['', 'activa', '']
+			break
+		case 3:
+			clases = ['', '', 'activa']
+			break
+		default:
+			clases = ['', '', '']
+	}
+
 	return (
 		<div className="fases">
-			{/*<div className="flecha-1 d-flex flex-row align-items-center">
-				<div className="cuerpo-flecha">
-					<p>Refuerzos</p>
-				</div>
-				<div className="punta-flecha"></div>
-			</div>
-
-			<div className="flecha-2 d-flex flex-row align-items-center">
-				<div className="pluma-flecha"></div>
-				<div className="cuerpo-flecha">
-					<p>Atacar</p>
-				</div>
-				<div className="punta-flecha"></div>
-			</div>
-			<div className="flecha-3 d-flex flex-row align-items-center">
-				<div className="pluma-flecha"></div>
-				<div className="cuerpo-flecha">
-					<p>Mover tropas</p>
-				</div>
-	</div>*/}
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
 				version="1.1"
@@ -35,20 +31,32 @@ const FasesPartida = () => {
 				<g id="layer1" transform="translate(0,-891.98765)">
 					<g transform="translate(121.78218,590.09901)" id="g4197">
 						<path
+							className={clases[0]}
 							id="path4168"
 							d="m 149.12106,351.1659 -250,0 0,70.73171 250,0 53.44551,-30.85678 z"
 							style={{ fill: '#444' }}
 						/>
+						<text style={{ fill: 'white' }} x="-10" y="393">
+							REFUERZOS
+						</text>
 						<path
+							className={clases[1]}
 							id="path4170"
 							d="m 468.42872,351.64983 53.69458,40.39408 -53.69458,30.54188 -249.7537,-0.49262 55.17241,-30.54187 -55.17241,-40.39409 z"
 							style={{ fill: '#444' }}
 						/>
+						<text style={{ fill: 'white' }} x="340" y="393">
+							ATAQUE
+						</text>
 						<path
+							className={clases[2]}
 							id="path4168-5"
 							d="m 789.44927,352.26436 -250,0 53.69458,40.29196 -53.69458,30.43975 250,0 z"
 							style={{ fill: '#444' }}
 						/>
+						<text style={{ fill: 'white' }} x="630" y="393">
+							MOVER TROPAS
+						</text>
 					</g>
 				</g>
 			</svg>
--- src/components/partida/Partida.js
+++ src/components/partida/Partida.js
@@ -307,7 +307,7 @@ export const Partida = () => {
 							</MDBBtn>
 						)}
 					</div>
-					<FasesPartida />
+					<FasesPartida fase={estado.fase} />
 				</>
 			)}
 
