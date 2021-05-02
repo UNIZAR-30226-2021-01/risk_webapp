4ee59ea985915fcfcd34e5318f7aac6e793ea6ce
alecron <alecronster@gmail.com>
2021-05-02 16:47:34 +0200

    arreglo error notificacion sala

--- src/components/panelNotificaciones/NotificacionSala.js
+++ src/components/panelNotificaciones/NotificacionSala.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
 import './notificacion.css'
 import { MDBRow, MDBCol, MDBBtn } from 'mdbreact'
 import { useHistory } from 'react-router-dom'
 import { rechazarPartida } from 'utils/restAPI'
+import { obtenerCredenciales } from 'utils/usuarioVO'
+import AuthApi from './../../utils/AuthApi'
 
 /**
  * Representación de una notificación de invitación a partida
@@ -11,6 +13,7 @@ import { rechazarPartida } from 'utils/restAPI'
  */
 export const NotificacionSala = ({ datos }) => {
 	const history = useHistory()
+	const Auth = useContext(AuthApi)
 
 	const decisionSala = (datos, decision) => async () => {
 		if (decision === 'aceptar') {
