445a1399520e865fa368cf1687e818b932b7ef24
alecron <alecronster@gmail.com>
2021-05-11 20:35:18 +0200

    Fondo reglas close #86 y corregido error de formato en RiskMapa

--- src/components/Reglas/Reglas.js
+++ src/components/Reglas/Reglas.js
@@ -12,42 +12,42 @@ import './Reglas.css'
  */
 export const Reglas = () => {
 	return (
-		<MDBContainer className="container-reglas">
-			<MDBRow>
-				<h1>Reglas pixelRISK</h1>
-			</MDBRow>
-			<h3 className="font-weight-bold mb-12 p-12">
-				<strong>Introducción:</strong>
-			</h3>
-			<p className="text-left">
-				En este juego participan entre 3 y 6 jugadores en un mapa global con el
-				objetivo de conquistar el mundo entero usando tropas para atacar y
-				defender territorios.
-			</p>
-			<p>Cada jugador tiene su turno, el cual está separado en fases:</p>
-			<h2>
-				<strong>Fase 1: </strong>
-			</h2>
-			<p>
-				Obtienes un numero de tropas nuevas dependiendo de los territorios que
-				tengas conquistados. Estas tropas se deben repartir como refuerzos en
-				territorios en los que ya tengas tropas.
-			</p>
-			<h2>
-				<strong>Fase 2: </strong>
-			</h2>
-			<p>
-				Ataca desde un territorio tuyo con un número específico de tropas a un
-				territorio enemigo tantas veces como quieras. No puedes atacar desde un
-				territorio que sólo tenga una tropa.
-			</p>
-			<h2>
-				<strong>Fase 3: </strong>
-			</h2>
-			<p>
-				Una única vez, mueve un número cualquiera de tropas desde un territorio
-				controlado por tí a otro territorio de tu propiedad.
-			</p>
+		<MDBContainer className="container-fondo-reglas">
+			<MDBContainer className="container-reglas">
+				<MDBRow>
+					<h1>Reglas pixelRISK</h1>
+				</MDBRow>
+				<h2 className="font-weight-bold mb-12 p-12">Introducción:</h2>
+				<p className="text-left">
+					En este juego participan entre 3 y 6 jugadores en un mapa global con
+					el objetivo de conquistar el mundo entero usando tropas para atacar y
+					defender territorios.
+				</p>
+				<p>Cada jugador tiene su turno, el cual está separado en fases:</p>
+				<h2>
+					<strong>Fase 1: </strong>
+				</h2>
+				<p>
+					Obtienes un numero de tropas nuevas dependiendo de los territorios que
+					tengas conquistados. Estas tropas se deben repartir como refuerzos en
+					territorios en los que ya tengas tropas.
+				</p>
+				<h2>
+					<strong>Fase 2: </strong>
+				</h2>
+				<p>
+					Ataca desde un territorio tuyo con un número específico de tropas a un
+					territorio enemigo tantas veces como quieras. No puedes atacar desde
+					un territorio que sólo tenga una tropa.
+				</p>
+				<h2>
+					<strong>Fase 3: </strong>
+				</h2>
+				<p>
+					Una única vez, mueve un número cualquiera de tropas desde un
+					territorio controlado por tí a otro territorio de tu propiedad.
+				</p>
+			</MDBContainer>
 		</MDBContainer>
 	)
 }
