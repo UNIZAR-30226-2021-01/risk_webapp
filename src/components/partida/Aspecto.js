070d5c23c851a7af15ccb8dab15abb39eba0b264
Daboposki <oscar@graus.biz>
2021-05-13 01:17:36 +0200

    Añadidos colores a tropas en partida

--- src/components/partida/Aspecto.js
+++ src/components/partida/Aspecto.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import tropas from 'assets/tropas/tropas'
+import tropasColor from 'assets/tropas/tropasColor'
+import './Aspecto.css'
 
-const Aspecto = ({ x, y, numAspecto }) => {
+const Aspecto = ({ x, y, numAspecto, jugador }) => {
+	console.log(tropasColor[numAspecto].img, 'aspecto')
 	return (
-		<image
-			href={tropas[numAspecto].img}
-			height="100px"
-			width="100px"
-			x={x - 35}
-			y={y - 50}
-		/>
+		<>
+			<image
+				href={tropas[numAspecto].img}
+				height="100px"
+				width="100px"
+				x={x - 35}
+				y={y - 50}
+			/>
+			<image
+				href={tropasColor[numAspecto].img}
+				className={`img_tropacolor_${jugador}`}
+				height="100px"
+				width="100px"
+				x={x - 35}
+				y={y - 50}
+			/>
+		</>
 	)
 }
 
